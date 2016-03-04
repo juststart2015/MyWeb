ff35a425014ba952f8745970d3c6aff253c729c2
juststart2015 <624993251@qq.com>
2016-03-04 09:16:18 +0800

    The first page

---
+++ MyWeb/contant/contant_curry.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link type="text/css" rel="stylesheet" href="styles/contant_curry.css" />
+<title>我的站</title>
+<style>
+	margin:0;
+	padding:0;
+</style>
+</head>
+
+<body>
+	<div class="main_box">
+		<div class="top_box">
+			<img src="images/GSW_logo.png" />
+			<h1>勇士|金州 Golden State Warriors</h1>
+			<p>2015-2016联赛 西部联盟排名第一</p>
+		</div>
+		<div class="center_box">
+			<div>
+				<ul>
+					<li>
+						<span>03-03</span>
+						<a href="http://nbachina.qq.com/a/20160303/003382.htm" target="_blank">
+							<h3>库里+勇士,双双入围劳伦斯 NBA历史获奖者仅一人</h3>
+						</a>
+					</li>
+					<li>
+						<span>03-03</span>
+						<a href="http://nbachina.qq.com/a/20160303/019592.htm" target="_blank">
+							<h3>视频-库里本赛季十大三分球 雷霆众将两当背景帝</h3>
+						</a>
+					</li>
+					<li>
+						<span>03-02</span>
+						<a href="http://nbachina.qq.com/a/20160302/041209.htm" target="_blank">
+							<h3>汤神26分格林准三双 库里一哥缺阵勇士加时胜老鹰</h3>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="bottom_box">
+			<h6>数据来源：<a href="http://china.nba.com/" target="_blank">NBA中国官方网站 china.nba.com</a></h6>
+		</div>
+	</div>
+</body>
+</html>
---
+++ MyWeb/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link type="text/css" rel="stylesheet" href="styles/main.css" />
+<title>我的站</title>
+</head>
+
+<body>
+	<div>
+		<div class="zhu_box" style="border:1px solid #000000;">
+			<div class="left_box" style="border:1px solid #000000">
+				<div class="top">
+					<h2>金的站 J'S站</h2>
+				</div>
+				<div class="contant">
+					<a href="contant/contant_curry.php" target="center_iframe">
+						<img src="images/left_1.jpg" />
+						Stephen Curry/金州勇士队
+					</a>
+				</div>
+				<div class="contant">
+					<a href="contant/contant_curry.php" target="center_iframe">
+						<img src="images/left_2.jpg" />
+						吮指原味鸡块
+					</a>
+				</div>
+			</div>
+			<div class="center_box" style="border:1px solid #000000">
+				<div class="top">
+					dingbu
+				</div>
+				<div>
+					<iframe name="center_iframe">
+						
+					</iframe>
+				</div>
+			</div>
+			<div class="right_box" style="border:1px solid #000000">
+				<div class="top">
+					<h6>
+						微信订阅号：金牌服务<br />
+						QQ群：88888888
+					</h6>
+				</div>
+				<div class="contant">
+					<img style="margin-top:16px" src="images/My_QR_Code.jpg" />
+					<img src="images/My_QQ_Code.jpg" />
+					扫描二维码，与我们在一起
+				</div>
+				<div>
+					neirong2
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
